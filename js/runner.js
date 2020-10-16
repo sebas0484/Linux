5b16384a56888d8c46dc7aa6f7439d61194998e9
sebas0484 <semuron.2@gmail.com>
2020-10-15 19:34:22 -0500

    Se agrega la logica necesaria para poder realizar el login de un usuario correctamente emulando una consola de linux

--- js/runner.js
+++ js/runner.js
@@ -1,16 +1,3 @@
-var usuarios=[
-    'Andres','Andrea','Carlos','Cristian',
-    'Daniel','Doris','Luna','Sara',
-    'Nicol','Felipe','Joan','Viviana',
-    'santiago','Lina','Laura','Luisa',
-];
-
-var maquinas=[
-    'Entrenadores',
-    'Estudiantes',
-    'Docentes',
-    'Universidad'
-];
 
 var ip=[
     '196.168.0.1',
@@ -18,14 +5,75 @@ var ip=[
     '196.168.0.3',
     '196.168.0.4'
 ]
+var flag=true;
+var flagLogin=true;
+var maquinaActual;
+var usuarioActual;
+function run() {
+    var maquinas = [
+        'Entrenadores',
+        'Estudiantes',
+        'Docentes',
+        'Universidad'
+    ];
+    var usuarios = [
+        'UsuarioRoot',
+        'Andres', 'Andrea', 'Carlos', 'Cristian',
+        'Daniel', 'Doris', 'Luna', 'Sara',
+        'Nicol', 'Felipe', 'Joan', 'Viviana',
+        'santiago', 'Lina', 'Laura', 'Luisa',
+    ];
 
-var comando=document.getElementById('comando');
+    if(flag==true){
+        console.log('Entro al if');
+        var msn='';
+        var comando=document.getElementById('comando');
+        msn=msn+comando.value+'\n';
+        comando.value='';
+         maquinaActual= msn;
+        document.getElementById('area').value='Ingrese comando: ';
+        flag=false;
+    }else{
+        console.log('Entro al else');
+        console.log('Envia: '+maquinaActual);
+        comandoIngresado(maquinaActual);
 
-function run(){
-    preguntarMaquina();
-    var maquinaActual=comando.value;
-
-    comando.value;
-    var separar=comando.split(' ');
-    login(separar[1]);
+    }
 }
+ function comandoIngresado(maquinaActual){
+     if(flagLogin==true){
+         console.log('Entro al metodo');
+         console.log('Recibe: '+maquinaActual);
+         var msn='';
+         var comando=document.getElementById('comando');
+         msn=msn+comando.value;
+         comando.value='';
+         console.log('Toma: '+msn);
+         var login='login';
+         if(msn===login){
+             console.log('Entro al metodo y al if');
+             flagLogin=false;
+         }else{
+             console.log('Entro al metodo y al else');
+         }
+
+         document.getElementById('area').value='Usuario: ';
+     }else{
+         loginUsuario();
+     }
+ }
+
+ function loginUsuario(){
+     var msn='';
+     var comando=document.getElementById('comando');
+     msn=msn+comando.value;
+     comando.value='';
+     var usr=msn;
+     usuarioActual=usr;
+     if(usr==='UsuarioRoot'){
+         msn='Bienvenido '+usr;
+         document.getElementById('area').value=msn;
+     }else{
+         document.getElementById('area').value='Usuario invalido';
+     }
+ }
